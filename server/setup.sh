414b872699efe55f9d96ed6fc0408dff3d8ae43b
Alexander Kurz <alexhkurz@gmail.com>
2023-10-14 19:56:43 -0700

    aider: Added /Users/akurz/alexhkurz-at-git/WikipediaGame/server/setup.sh

---
+++ server/setup.sh
@@ -0,0 +1,10 @@
+ #!/bin/bash
+
+ # Create the virtual environment
+ python3.10 -m venv venv
+
+ # Activate the virtual environment
+ source venv/bin/activate
+
+ # Install the dependencies
+ pip install -r requirements.txt
