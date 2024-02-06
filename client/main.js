3a63c8b71dc20f1a90c3222cd09e47edaaa7f943
Alexander Kurz <alexhkurz@gmail.com>
2024-02-05 23:18:04 -0800

    Added error message and elapsed time to logs in client main.js.

--- client/main.js
+++ client/main.js
@@ -24,6 +24,7 @@ document.getElementById('wiki-form').addEventListener('submit', function(event) 
     })
     .catch(error => {
         console.error('Error:', error);
+        // output discovered pages 
         var logsElement = document.getElementById('logs');
         logsElement.innerHTML = error.message + (data && data.time ? '<p>Elapsed time: ' + data.time + '</p>' : '');
         var pathElement = document.getElementById('path');
